7ac23efb2397490c69a3d6594a7b0b59119c54f4
Jaime Still <jpstill85@gmail.com>
2021-04-11 10:20:48 -0400

    ch.5 complete

--- 05-if-statements/main.py
+++ 05-if-statements/main.py
@@ -79,3 +79,27 @@ if requested_toppings:
     print('Finished making your pizza!\n')
 else:
     print('Are you sure you want a plain pizza?\n')
+
+# using multiple lists
+
+available_toppings = [
+    'mushrooms',
+    'olives',
+    'green peppers',
+    'pepperoni',
+    'pineapple',
+    'extra cheese'
+]
+
+requested_toppings = [
+    'mushrooms',
+    'french fries',
+    'extra cheese'
+]
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f'Adding {requested_topping}.')
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print('Finished making your pizza!\n')
